1d299433c4ccd783687d56e7770072213ecfc1df
Huynh Huu My <my.huynh@sutrixsolutions.com>
2018-01-15 18:12:39 +0700

    Create simple component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+// import { AppComponent } from './app.component';
+import { WordComponent } from './word.component';
+// import { WordComponent } from './word/WordComponent.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    // AppComponent,
+    WordComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  // bootstrap: [AppComponent]
+  bootstrap: [WordComponent]
 })
 export class AppModule { }
