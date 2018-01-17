d979eaa90b64c0e35b949abbc2b0dd5d14bc176e
Huynh Huu My <my.huynh@sutrixsolutions.com>
2018-01-17 18:24:58 +0700

    using @ViewChild change value child component

--- src/app/child.component.ts
+++ src/app/child.component.ts
@@ -5,10 +5,18 @@ import { Component, Output, EventEmitter } from '@angular/core';
   template: `<button (click)="addForParent();" ion-button>Add</button>
     <br>
     <button (click)="subForParent();" ion-button>Sub</button>
+
+    <br>
+    <h2>Chang value child</h2>
+    <br>
+    <h3> {{ value }} </h3>
   `
 })
 
 export class ChildComponent {
+
+  value = 0;
+
   @Output() myClick = new EventEmitter<boolean>();
 
   addForParent() {
--- src/app/parent.component.ts
+++ src/app/parent.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { ChildComponent } from './child.component';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-parent',
-  template: `<h3> {{value}} </h3>
-            <app-child (myClick)="changeValue($event);" ></app-child>
+  template: `
+            <h2>Change value parent</h2>
+            <h3> {{value}} </h3>
+            <app-child #child (myClick)="changeValue($event);" ></app-child>
+            <br>
+            <button (click)="onAddForChild();" ion-button>Add for child</button>
           `
 })
 
@@ -12,5 +17,12 @@ export class ParentCompnent {
 
   changeValue(isAdd: boolean) {
     isAdd === true ? this.value = this.value + 1 : this.value = this.value - 1;
+  }
+
+  @ViewChild(ChildComponent)
+  myChild : ChildComponent;
+
+  onAddForChild() {
+    this.myChild.value++;
   }
 }
